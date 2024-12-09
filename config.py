ce2de372037442064206f21b1fbd048477674d30
Fabio Lapa <70061490+suitablychosenusername@users.noreply.github.com>
2024-12-09 09:55:47 -0300

    add env.example

--- config.py
+++ config.py
@@ -6,31 +6,31 @@ import os
 dotenv.load_dotenv('./.env')
 
 class DatabaseSettings(BaseSettings):
-    CONNECTOR: str = os.getenv('DATABASE_CONNECTOR')
-    HOST: str = os.getenv('DATABASE_HOST')
-    PORT: str = os.getenv('DATABASE_PORT')
-    DB_NAME: str = os.getenv('DATABASE_DB_NAME')
-    DB_USER: str = os.getenv('DATABASE_USER')
+    CONNECTOR: str   = os.getenv('DATABASE_CONNECTOR')
+    HOST: str        = os.getenv('DATABASE_HOST')
+    PORT: str        = os.getenv('DATABASE_PORT')
+    DB_NAME: str     = os.getenv('DATABASE_DB_NAME')
+    DB_USER: str     = os.getenv('DATABASE_USER')
     DB_PASSWORD: str = os.getenv('DATABASE_PASSWORD')
 
 class AuthenticationSettings(BaseSettings):
     SECRET_KEY: str = os.getenv('SECRET_KEY')
-    ALGORITHM: str = os.getenv('ALGORITHM')
+    ALGORITHM: str  = os.getenv('ALGORITHM')
     ACCESS_TOKEN_EXPIRE_MINUTES: int = os.getenv('ACCESS_TOKEN_EXPIRE_MINUTES', 30)
 
 class CORSSettings(BaseSettings):
-    ALLOWED_ORIGINS: list[str] = [
-        'http://localhost',
-        'https://localhost'
-    ]
+    allowed_origins_raw = os.getenv(
+        'ALLOWED_ORIGINS', 'http://localhost,https://localhost'
+    )
+    ALLOWED_ORIGINS: list[str] = allowed_origins_raw.split(',')
 
 class Settings(BaseSettings):
     DEBUG: bool = os.getenv('DEBUG', True)
     APP_NAME: str = os.getenv('APP_NAME', 'CoPR - Contest Problem Radar')
     
     auth: AuthenticationSettings = AuthenticationSettings()
-    cors: CORSSettings = CORSSettings()
-    database: DatabaseSettings = DatabaseSettings()
+    cors: CORSSettings           = CORSSettings()
+    database: DatabaseSettings   = DatabaseSettings()
 
 # @lru_cache
 # def get_settings():
